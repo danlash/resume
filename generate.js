d53f838fc5411fff9218d4f64cc2d018add4291d
Dan Lash <danlash@gmail.com>
2014-03-08 18:26:28 -0500

    add education, soften edges of raw data

--- generate.js
+++ generate.js
@@ -35,6 +35,7 @@ var technologyGroups = _.groupBy(technologies, function(technology){
 });
 
 technologyGroups = _.map(technologyGroups, function(technologies, name){
+  technologies = _.sortBy(technologies, function(technology) { return technology.name.toLowerCase(); })
   return { type: name, technologies: technologies };
 });
 
@@ -42,7 +43,43 @@ _.each(projects, function(project){
   var employer = _.find(employers, function(e) { return e.name === project.employer; });
   employer.highlights = employer.highlights || [];
   employer.highlights = employer.highlights.concat(project.highlights);
+
+  _.each(project.technologies, function(pt){
+    if (!_.find(technologies, function(t){return t.name === pt})) 
+      console.log('\
+  {\n\
+    "name"  :       "'+pt+'",\n\
+    "type"  :       "",\n\
+    "proficiency" : 0,\n\
+    "affinity"  :   0\n\
+  },')
+
+  });
 });  
+
+var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+
+function englishizeDates(collection) {
+  _.each(collection, function(item) {
+    var start = new Date(item.start);
+    item.start = months[start.getUTCMonth()] + ' ' + start.getUTCFullYear();
+    var end = new Date(item.end);
+    item.end = months[end.getUTCMonth()] + ' ' + end.getUTCFullYear();
+    
+  });
+}
+
+englishizeDates(employers);
+englishizeDates(degrees);
+
+function sortDescending(collection) {
+  return _.sortBy(collection, function(item){
+    return (new Date(item.start)).toISOString();
+  }).reverse();
+}
+
+employers = sortDescending(employers);
 
 less.render(lessSource, {compress:true}, function (err, css) {
   if (err) throw err;
@@ -56,7 +93,8 @@ less.render(lessSource, {compress:true}, function (err, css) {
     projects: projects,
     technologies: technologies,
     technologyGroups: technologyGroups,
-    employers: employers
+    employers: employers,
+    degrees: degrees
   };
 
   var result = template(data);
